6134f842f747e91f9a43a5866d39f30b0356b65c
Losig23 <anirudhyarram@gmail.com>
2026-01-19 21:37:30 -0600

    Add User model and users table

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -7,6 +7,7 @@ from sqlalchemy import pool
 from app.config import settings
 
 from alembic import context
+from app import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
